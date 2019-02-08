2934ceda6d50d126f1073ef54fb3869ec2d7f090
Артур Булатов <a.r.bulatov@tinkoff.ru>
2019-02-08 22:49:52 +0300

    Начало AuthenticationService'a

---
+++ src/Recommendations.API/Recommendations.API/Services/AuthenticationService.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Recommendations.API.Model.Settings;
+using Recommendations.DB;
+
+namespace Recommendations.API.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<string> Authenticate(string login, string password);
+    }
+
+    public class AuthenticationService
+    {
+        readonly IOptions<GlobalOptions> _options;
+        readonly DBClient _client;
+
+        public AuthenticationService(IOptions<GlobalOptions> options, DBClient client)
+        {
+            _options = options;
+            _client = client;
+        }
+
+        public async Task<string> Authenticate(string login, string password)
+        {
+
+        }
+    }
+}
